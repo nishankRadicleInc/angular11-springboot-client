a8f81c85b848c13b615e4bbb23f12cce2cd6b49b
nishankRadicleInc <nsinghal@radicleinc.com>
2021-03-09 15:46:56 +0530

    first commit

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<angular11-springboot-client>'));
+app.use(express.static(__dirname + '/dist/angular11-springboot-client'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<angular11-springboot-client>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/angular11-springboot-client/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
