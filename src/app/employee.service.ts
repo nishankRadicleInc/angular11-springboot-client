e4367d62d94a08c0ceee8fbae5fa27a2250f83c9
nishankRadicleInc <nsinghal@radicleinc.com>
2021-03-09 21:59:09 +0530

    first commit

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class EmployeeService {
 
-  private baseUrl = 'http://localhost:8080/springboot-crud-rest/api/v1/employees';
+  private baseUrl = 'https://javaspring123.herokuapp.com/api/v1/employees';
 
   constructor(private http: HttpClient) { }
 
